ad79993725932935b3b0d7df43783912f49098d0
aleebrady <aleebrady@gmail.com>
2021-04-02 15:46:19 -0700

    setup axios fetch

--- src/redux/reducers/productReducer.js
+++ src/redux/reducers/productReducer.js
@@ -1,18 +1,12 @@
 import { ActionTypes } from "../constants/action-types"; // reducer always takes the initial state and action 
 
 const initialState = {
-    products: [
-        {
-            id: 1,
-            title: "Knee Sleeves",
-            category: "fitness equipment"
-        },
-    ]
+    products: []
 }
 export const productReducer = (state = initialState, {type, payload}) => {
     switch (type) {
         case ActionTypes.SET_PRODUCTS:
-            return state
+            return { ...state, products: payload }
         default:
             return state
     }
